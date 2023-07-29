7b8e8e0b8e9864d590d217241200560388e5a4c2
Mehedi Hasan <mehedihasan@Mehedis-MacBook-Pro.local>
2023-07-29 09:32:00 +0600

    feat: featured product done
     made it with SSG

---
+++ src/components/HomePage/Featured.js
@@ -0,0 +1,85 @@
+import Link from "next/link";
+
+const getRandomProducts = (data, count) => {
+  const shuffledData = data.sort(() => 0.5 - Math.random());
+  return shuffledData.slice(0, count);
+};
+
+const Featured = ({randomProducts}) => {
+  // const [featuredProducts, setFeaturedProducts] = useState([]);
+
+  // useEffect(() => {
+  //   Fetch all products from the API endpoint
+  //   fetch("http://localhost:3000/api/pc")
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       // Randomly select 6 products from the fetched data
+  //       const randomProducts = getRandomProducts(data, 6);
+  //       console.log(randomProducts);
+  //       setFeaturedProducts(randomProducts);
+  //     })
+  //     .catch((error) => {
+  //       console.error("Error fetching products:", error);
+  //     });
+
+  //   const fetchData = async () => {
+  //     try {
+  //       const response = await fetch("http://localhost:3000/api/pc");
+  //       const data = await response.json();
+  //       const randomProducts = getRandomProducts(data.data, 6);
+  //       console.log("data fetching complete", data.data);
+  //       setFeaturedProducts(randomProducts);
+  //     } catch (error) {
+  //       console.error("Error fetching data:", error);
+  //     }
+  //   };
+
+  //   // Call the fetchData function when the component is mounted
+  //   fetchData();
+  // }, []);
+
+
+  return (
+    <div>
+      <h1>Featured Products</h1>
+      <div>
+        {randomProducts?.map((product) => (
+          <div
+            key={product._id}
+            style={{
+              border: "1px solid #ccc",
+              padding: "10px",
+              margin: "10px",
+            }}
+          >
+            <img
+              src={product.Image}
+              alt={product.ProductName}
+              style={{ width: "200px", height: "200px" }}
+            />
+            <h3>{product.ProductName}</h3>
+            <p>Category: {product.Category}</p>
+            <p>Price: ${product.Price}</p>
+            <p>Status: {product.Status}</p>
+            <p>Rating: {product.Rating} out of 5 stars</p>
+            <Link href={`/products/${product._id}`}>View Details</Link>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Featured;
+
+export const getStaticProps = async () => {
+  const response = await fetch("http://localhost:3000/api/pc");
+  const data = await response.json();
+  const randomProducts = getRandomProducts(data.data, 6);
+  console.log("data fetching complete", data.data);
+  return {
+    props: {
+      randomProducts
+    },
+  };
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,14 @@
 import auth from "@/firebase/firebase.auth.js";
 import { useSession } from "next-auth/react";
-import Head from "next/head";
+import Link from "next/link";
 import { useAuthState } from "react-firebase-hooks/auth";
-const HomePage = () => {
+
+const getRandomProducts = (data, count) => {
+  const shuffledData = data.sort(() => 0.5 - Math.random());
+  return shuffledData.slice(0, count);
+};
+
+const HomePage = ({ randomProducts }) => {
   const { data: session } = useSession();
 
   const [user, loading, error] = useAuthState(auth);
@@ -10,23 +16,42 @@ const HomePage = () => {
   console.log("From home", user);
 
   return (
-    <div>
-      <Head>
-        <title>Next Auth</title>
-      </Head>
-      <h1 style={{ textAlign: "center", marginTop: "10%" }}>
-        Welcome To Next Auth Home Page
-      </h1>
-      <h2 style={{ textAlign: "center" }}>
-        Logged in user: {session?.user?.name}
-      </h2>
-      {session?.user?.email && (
-        <h2 style={{ textAlign: "center" }}>
-          Logged in user email: {session?.user?.email}
-        </h2>
-      )}
+    <div className="grid gap-4">
+      <h1 className="text-2xl font-bold text-center">Featured Products</h1>
+      <div className="grid grid-cols-1 gap-4 sm:grid-cols-3">
+        {randomProducts?.map((product) => (
+          <div
+            key={product._id}
+            className="border border-gray-300 p-4 text-center rounded-lg"
+          >
+            <img
+              src={product.Image}
+              alt={product.ProductName}
+              className="w-40 h-40 mx-auto mb-4"
+            />
+            <h3 className="text-xl font-semibold">{product.ProductName}</h3>
+            <p>Category: {product.Category}</p>
+            <p>Price: ${product.Price}</p>
+            <p>Status: {product.Status}</p>
+            <p>Rating: {product.Rating} out of 5 stars</p>
+            <Link href={`/products/${product._id}`}>View Details</Link>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
 
 export default HomePage;
+
+export const getStaticProps = async () => {
+  const response = await fetch("http://localhost:3000/api/pc");
+  const data = await response.json();
+  const randomProducts = getRandomProducts(data.data, 6); /* data.data */
+  console.log("data fetching complete", data.data);
+  return {
+    props: {
+      randomProducts,
+    },
+  };
+};
