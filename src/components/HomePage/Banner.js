d33498d5d50886b434f2cb4020e25d8763791b1d
Mehedi Hasan <mehedihasan@Mehedis-MacBook-Pro.local>
2023-07-30 15:39:04 +0600

    feat: banner made mobile responsive

--- src/components/HomePage/Banner.js
+++ src/components/HomePage/Banner.js
@@ -4,8 +4,8 @@ const Banner = () => {
   return (
     <div className="bg-gradient-to-r from-yellow-400 via-yellow-500 to-yellow-600 text-white">
       <div className="container mx-auto py-4">
-        <div className="flex justify-between items-center">
-          <div className="flex items-center">
+        <div className="flex flex-col md:flex-row justify-between items-center">
+          <div className="flex items-center mb-4 md:mb-0">
             <img
               src="https://media.istockphoto.com/id/1309717274/vector/realistic-black-modern-thin-frame-display-computer-monitor-vector-illustration-jpg.jpg?s=612x612&w=0&k=20&c=hWFdkv0V09HqWjqRy2w93ikw2RBAcoxrhXq_9AQsOhQ="
               alt="Amazon Logo"
@@ -13,12 +13,17 @@ const Banner = () => {
             />
             <h1 className="text-xl font-semibold">PC assembler</h1>
           </div>
-          <Link className="text-xl font-semibold" href="#featured-cat">
-            Build By Category
-          </Link>
+          <div className="md:ml-4">
+            <Link className="text-xl font-semibold block md:inline-block md:mt-0" href="#featured-cat">
+              Build By Category
+            </Link>
+          </div>
         </div>
         <p className="text-sm mt-1">Discover the best of functionality and versatility</p>
-        <Link className="bg-yellow-800 text-white py-2 px-4 rounded-md mt-4 font-semibold inline-block"  href="#featured-cat">
+        <Link
+          className="bg-yellow-800 text-white py-2 px-4 rounded-md mt-4 font-semibold inline-block"
+          href="#featured-cat"
+        >
           Explore Now
         </Link>
       </div>
